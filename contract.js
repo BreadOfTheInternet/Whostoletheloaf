af135214b37064ce92155b21c803ab1504125a8f
Bread Of The Internet <slicedbreadcoin@outlook.com>
2025-06-26 00:13:59 -0700

    Update contract.js

--- contract.js
+++ contract.js
@@ -1,5 +1,5 @@
 // contract.js
-const CONTRACT_ADDRESS = "0x046fe62bc4dce3c9b255c48a69f28cb795a418a0";
+const CONTRACT_ADDRESS = "0x403C73A921FB11375816d4F035FC1711229F2b7F"; // New live contract
 const CONTRACT_ABI = [
   {
     "inputs": [
@@ -8,6 +8,27 @@ const CONTRACT_ABI = [
     "name": "joinGame",
     "outputs": [],
     "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getRegions",
+    "outputs": [{ "internalType": "string[]", "name": "", "type": "string[]" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "string", "name": "region", "type": "string" }],
+    "name": "getHolderCount",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "week", "type": "uint256" }],
+    "name": "getWeeklyThreshold",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "pure",
     "type": "function"
   }
 ];
